3ee7160aa5b7af7ada0ff8fcd634f31b318447ca
Alexander Deider (home) <arth.inbox@gmail.com>
2018-11-07 02:32:24 +0700

    Implicit Intent (open default browser by https:// uri)

--- app/src/main/java/info/vparth/helloworld/helloworld/MainActivity.java
+++ app/src/main/java/info/vparth/helloworld/helloworld/MainActivity.java
@@ -1,7 +1,11 @@
 package info.vparth.helloworld.helloworld;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.browseSOButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://stackoverflow.com"));
+                startActivity(intent);
+            }
+        });
     }
 }
